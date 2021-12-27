e08ebe2f0bb5bdb95ad2e6cf2f3604fd60b7802a
Vincent <antoniod877@gmail.com>
2021-12-27 15:53:46 +0100

    Update group class

--- src/fr/projetl3s5/groups/Group.java
+++ src/fr/projetl3s5/groups/Group.java
@@ -1,8 +1,11 @@
 package fr.projetl3s5.groups;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Group {
 
-	ELEVE(1),;
+	ELEVE(1);
 
 	private int id;
 	
@@ -10,11 +13,13 @@ public enum Group {
 		this.id = id;
 	}
 	
-	public static Group getGroupByID(int int1) {
+	public static List<Group> getGroupByID(int int1) {
+		List<Group> list = new ArrayList<>();
+		
 		for(Group g : Group.values()) {
-			if(int1 == g.id)return g;
+			if((int1 & g.id) != 0) list.add(g);
 		}
-		return null;
-	} 
+		return list;
+	}
 	
 }
