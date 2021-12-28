7de0d49530b599544ecb76fe6688d57a1d16b4e1
Vincent <antoniod877@gmail.com>
2021-12-28 18:36:28 +0100

    Refactoring user

--- src/fr/projetl3s5/ui/User.java
+++ src/fr/projetl3s5/ui/User.java
@@ -8,73 +8,68 @@ import java.util.TreeMap;
 import fr.projetl3s5.groups.Group;
 
 public class User {
-	private String id;
-	private String nom;
-	private String prenom;
-	private int nbTicketsTotal=0;
 	
+	private String id, name, fName;
+	private int nbTotalTicket = 0;
+
 	private List<String> groupe = new ArrayList<>();
 	private NavigableMap<String, List<Ticket>> listTickets = new TreeMap<>((String g1, String g2) -> g1.compareTo(g2));
-	
-	
-	public User(String id, String nom, String prenom, Integer groupe) {
-		this.id=id;
-		this.prenom=prenom;
-		this.nom=nom;
-		
-		for(Group g : Group.getGroupsByID(groupe)) {
+
+	public User(String id, String name, String fName, Integer group) {
+		this.id = id;
+		this.fName = fName;
+		this.name = name;
+
+		for (Group g : Group.getGroupsByID(group)) {
 			this.groupe.add(g.toString());
 		}
-		
-		for(Group g : Group.values()) {
+
+		for (Group g : Group.values()) {
 			listTickets.put(g.toString(), new ArrayList<>());
 		}
 	}
 
-	
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getPrenom() {
-		return prenom;
+		return fName;
 	}
-	
+
 	public String getNom() {
-		return nom;
+		return name;
 	}
-	
+
 	public List<String> getGroupe() {
 		return groupe;
 	}
-	
+
 	public int getNbTicketsTotal() {
-		return nbTicketsTotal;
+		return nbTotalTicket;
 	}
-	
+
 	public NavigableMap<String, List<Ticket>> getListTicket() {
 		return listTickets;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 
-		if(obj instanceof User) {
-			User caca = (User) obj;
-			return id.equals(caca.getId()) && 
-					prenom.equals(caca.getPrenom()) && nom.equals(caca.getNom())
-					&& groupe == caca.getGroupe();
+		if (obj instanceof User) {
+			User user = (User) obj;
+			return id.equals(user.getId()) && fName.equals(user.getPrenom()) && name.equals(user.getNom())
+					&& groupe.equals(user.getGroupe());
 		}
-		
-	
+
 		return false;
 	}
-	
-	public void setListTicket(Ticket t) {
+
+	public void setTicketList(Ticket t) {
 		String g = t.getGroupe();
-		if(groupe.contains(g)) {
+		if (groupe.contains(g)) {
 			listTickets.get(g).add(t);
-			nbTicketsTotal++;
+			nbTotalTicket++;
 		}
 	}
 }
